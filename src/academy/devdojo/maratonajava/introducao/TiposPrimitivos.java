5664ac7d9ddc8bf127a0582d1d6d473dd95472eb
Othavio Nogueira <othavionogueira2003@gmail.com>
2026-08-05 18:03:52 -0300

    primitive types

---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        // int, double, float, char, byte, short, long e boolean
+
+        int age = 23;
+        long numberStronger = 100000;
+        double salaryDouble = 3000.00;
+        float salaryFloat = 3000.0f;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean afirmative = true;
+        boolean negative = false;
+        String nome = "Othavio";
+        var nome2 = "Oliver Sykes";
+        char character = 'A';
+
+        System.out.println("my age is" + age);
+        System.out.println("my actual salary is" + salaryFloat);
+    }
+}
